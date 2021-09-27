591cb9f9771f5b15a6bf11866a9e80d27ab1e8dc
alexmagwe <alexmagwe@gmail.com>
2021-09-27 20:38:40 +0300

    added basic pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Navigation from '../components/navigation/Navigation'
+import { ThemeProvider } from 'styled-components'
+import {Provider} from 'next-auth/client'
+import { getSession } from 'next-auth/client'
 
+const theme = {
+  colors: {
+    primary: "#E0FBFF",
+    secondary: "#0910D0",
+    accent: '#77059F',
+  }
+}
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Provider>
+        <Navigation />
+        <Component {...pageProps} />
+      </Provider>
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
