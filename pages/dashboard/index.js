4fb1765113b8976f47961e6545aa00abc39f9360
alexmagwe <alexmagwe@gmail.com>
2021-12-19 13:01:31 +0300

    initial dashboard build

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,29 +1,40 @@
-import React,{useEffect} from 'react'
-import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0';
-import { useSelector,useDispatch } from 'react-redux';
-import { fetchUserDetails } from '../../redux/user/userActions';
-import { fetchCourseInfo } from '../../redux/reducers/units/unitActions';
+import React, { useEffect } from "react";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUserDetails } from "../../redux/user/userActions";
+import { fetchCourseInfo } from "../../redux/reducers/units/unitActions";
+import DashNavbar from "../../components/dashboard/DashNavbar";
+import { Skeleton } from "react-loading-skeleton";
+import Main from "../../components/dashboard/Main";
 function Dashboard() {
-    const { user, } = useUser();
-    const userInfo = useSelector(state => state.userInfo)
-    const dispatch = useDispatch()
-    const { loading, shouldUpdateCourse, shouldUpdateSemester, shouldUpdateYear, role, course, year, semester } = userInfo
-    useEffect(() => {
-        if(user){
-            dispatch(fetchUserDetails(user))
-    dispatch(fetchCourseInfo(user))
-        }
-
-    }, [dispatch,user])
-    return (<div>
-        {
-            !loading ? (<div>
-                <h1>Welcome to your personal Dashboard {user && user.nickname}</h1>
-            </div>) : <h1>Loading...</h1>
-        }
+  const { user } = useUser();
+  const userInfo = useSelector((state) => state.userInfo);
+  const dispatch = useDispatch();
+  const {
+    loading,
+    shouldUpdateCourse,
+    shouldUpdateSemester,
+    shouldUpdateYear,
+    role,
+    course,
+    year,
+    semester,
+  } = userInfo;
+  useEffect(() => {
+    if (user) {
+      dispatch(fetchUserDetails(user));
+      dispatch(fetchCourseInfo(user));
+    }
+  }, [dispatch, user]);
+  return (
+    <div>
+      {/* <DashNavbar brandText={"Laibu"} user={user}/> */}
+      {!loading ? <div>
+      <Main/>
+      </div> : <Skeleton />}
     </div>
-    )
+  );
 }
 
-export default Dashboard
-export const getServerSideProps = withPageAuthRequired();
+export default Dashboard;
+// export const getServerSideProps = withPageAuthRequired();
